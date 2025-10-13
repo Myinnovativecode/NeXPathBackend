d3083fdd16284465e6fcb26f6ab759dd6b7b460e
Janvi Prajapat <janviprajapat18022004@gmail.com>
2025-10-13 22:01:02 +0530

    main.py changed

--- backend/main.py
+++ backend/main.py
@@ -55,10 +55,15 @@ app.include_router(resume_router)
 app.include_router(user_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+origins = [
+    "https://nexpath.vercel.app/",  # replace with your actual Vercel domain
+    "http://localhost:5173",             # dev
+]
+
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
